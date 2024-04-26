488edb645a805b52fc5568614cb6394cb786944f
DaanMichielsen <r0896365@student.thomasmore.be>
2024-04-26 20:34:11 +0200

    Prisma schema and init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { ThemeToggle } from "~/components/ui/theme-toggle";
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen">
       <ThemeToggle />
     </main>
   );
